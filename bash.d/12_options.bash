d6ae918fedbf04f55d74124949895e973fdab0f3
Victor Koronen <koronen@kth.se>
2012-04-25 15:44:21 +0200

    Add environment config and change load order

--- bash.d/12_options.bash
+++ bash.d/12_options.bash
@@ -2,12 +2,12 @@
 # ... or force ignoredups and ignorespace
 HISTCONTROL=ignoredups:ignorespace
 
-# append to the history file, don't overwrite it
-shopt -s histappend
-
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
+
+# append to the history file, don't overwrite it
+shopt -s histappend
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
