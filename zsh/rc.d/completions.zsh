89edbddd65b5e129a2b5d8e9401bfc4e6b1b28ec
Victor Koronen <koronen@kth.se>
2014-01-06 18:38:33 +0100

    Style zsh completions
    Source: <https://github.com/sorin-ionescu/prezto/blob/391bc405d0378d092937f62944390b79edb13ad9/modules/completion/init.zsh#L50-L70>

--- zsh/rc.d/completions.zsh
+++ zsh/rc.d/completions.zsh
@@ -1,2 +1,24 @@
 fpath=($HOME/.zsh/bundle/zsh-completions/src $fpath)
 autoload -Uz compinit && compinit -i
+
+# Group matches and describe.
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# Increase the number of errors based on the length of the typed word.
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
