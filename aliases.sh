d50feb5eb34a7df697507383eea28983f3d3fa3a
Victor Koronen <koronen@kth.se>
2014-01-06 18:10:33 +0100

    Add pag alias for `ps aux | grep -i`
    Source: <https://github.com/Frost/dotfiles/blob/5ed1548fa8ee7abe4279b3744d5a0b6f3bfb61e5/zsh/parts/aliases.zsh#L24>

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,7 @@
 alias ..='cd ..'
 alias ack='ack-grep'
 alias dbus-suspend='dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower org.freedesktop.UPower.Suspend'
+alias pag='ps aux | grep -i'
 alias trm='rm -f **/*~'
 alias up='sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y'
 
