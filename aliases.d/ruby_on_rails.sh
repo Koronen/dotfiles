fe58076a1bd8fc52f3fc531025d5d3042832b31b
Victor Koronen <koronen@kth.se>
2014-10-26 13:54:43 +0100

    Make Ruby on Rails aliases binstub-aware

--- aliases.d/ruby_on_rails.sh
+++ aliases.d/ruby_on_rails.sh
@@ -1,4 +1,31 @@
-alias rorc='bundle exec rails console'
-alias rordm='bundle exec rake db:migrate'
-alias rorro='bundle exec rake routes'
-alias rors='bundle exec rails server --binding 127.0.0.1'
+function rorc {
+    if [ -x bin/rails ]; then
+        bin/rails console
+    else
+        bundle exec rails console
+    fi
+}
+
+function rordm {
+    if [ -x bin/rake ]; then
+        bin/rake db:migrate
+    else
+        bundle exec rake db:migrate
+    fi
+}
+
+function rorro {
+    if [ -x bin/rake ]; then
+        bin/rake routes
+    else
+        bundle exec rake routes
+    fi
+}
+
+function rors {
+    if [ -x bin/rails ]; then
+        bin/rails server --binding 127.0.0.1
+    else
+        bundle exec rails server --binding 127.0.0.1
+    fi
+}
