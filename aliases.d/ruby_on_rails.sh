e5b8a69c23f9c4bc91fb2ecd00775aba03cd0e9d
Victor Koronen <koronen@kth.se>
2014-10-28 15:28:55 +0100

    Pass arguments through in Ruby on Rails aliases

--- aliases.d/ruby_on_rails.sh
+++ aliases.d/ruby_on_rails.sh
@@ -1,31 +1,31 @@
 function rorc {
     if [ -x bin/rails ]; then
-        bin/rails console
+        bin/rails console $@
     else
-        bundle exec rails console
+        bundle exec rails console $@
     fi
 }
 
 function rordm {
     if [ -x bin/rake ]; then
-        bin/rake db:migrate
+        bin/rake db:migrate $@
     else
-        bundle exec rake db:migrate
+        bundle exec rake db:migrate $@
     fi
 }
 
 function rorro {
     if [ -x bin/rake ]; then
-        bin/rake routes
+        bin/rake routes $@
     else
-        bundle exec rake routes
+        bundle exec rake routes $@
     fi
 }
 
 function rors {
     if [ -x bin/rails ]; then
-        bin/rails server --binding 127.0.0.1
+        bin/rails server --binding 127.0.0.1 $@
     else
-        bundle exec rails server --binding 127.0.0.1
+        bundle exec rails server --binding 127.0.0.1 $@
     fi
 }
