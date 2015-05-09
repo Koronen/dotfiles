ab3ae8e7343606e43023709e45ea93215f5c3f9c
Victor Koronen <koronen@kth.se>
2015-05-09 15:14:27 +0200

    Add return code to Bash prompt

--- bash/rc.d/prompt.bash
+++ bash/rc.d/prompt.bash
@@ -1,4 +1,4 @@
-PS1=''
+PS1="\$( ret=\$?; [[ \$ret -ne 0 ]] && echo \"\[\e[0;31m\]\$ret\[\e[0m\] \" )"
 [[ -n "$SSH_CONNECTION" ]] && PS1+='\h:'
 PS1+='\w'
 __git_ps1 > /dev/null 2>&1 && PS1+='$(__git_ps1)'
