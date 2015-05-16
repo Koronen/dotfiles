e9dc893d6daf195cdcd3b43ba822bb6b1afa9795
Victor Koronen <koronen@kth.se>
2015-05-16 14:34:52 +0200

    Fix Bash prompt on strigiformes
    GNU bash version 2.05 doesn't support `+=`.

--- bash/rc.d/prompt.bash
+++ bash/rc.d/prompt.bash
@@ -1,5 +1,5 @@
 PS1="\$( ret=\$?; [[ \$ret -ne 0 ]] && echo \"\[\e[0;31m\]\$ret\[\e[0m\] \" )"
-[[ -n "$SSH_CONNECTION" ]] && PS1+='\h:'
-PS1+='\w'
-__git_ps1 > /dev/null 2>&1 && PS1+='$(__git_ps1)'
-PS1+='\$ '
+[[ -n "$SSH_CONNECTION" ]] && PS1="${PS1}\\h:"
+PS1="${PS1}\\w"
+__git_ps1 > /dev/null 2>&1 && PS1="${PS1}\$(__git_ps1)"
+PS1="${PS1}\\$ "
