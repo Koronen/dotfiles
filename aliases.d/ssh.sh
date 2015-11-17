056182a29dd7ea85c9deb3b807a8e155575ae89b
Victor Koronen <koronen@kth.se>
2015-11-17 15:55:38 +0100

    Complete the `tssh` alias as `ssh`
    This change makes completion for this alias actually work.

--- aliases.d/ssh.sh
+++ aliases.d/ssh.sh
@@ -2,5 +2,5 @@ tssh() {
     ssh -At "$@" -- "[ -f koronen.tmux ] || wget -O koronen.tmux https://raw.githubusercontent.com/Koronen/dotfiles/master/tmux.conf; tmux -f koronen.tmux -L koronen.tmux new-session -A -s 0"
 }
 if type compdef > /dev/null 2>&1; then
-  compdef _ssh tssh=ssh
+  compdef tssh=ssh
 fi
