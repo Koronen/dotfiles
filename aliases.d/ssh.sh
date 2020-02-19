e285b2bbca25b1d90c48898081ce3a20e0958a16
Victor Koronen <git@victorkoronen.se>
2020-02-19 14:36:57 +0100

    Remove unused 'tssh' alias

--- aliases.d/ssh.sh
+++
@@ -1,6 +0,0 @@
-tssh() {
-    ssh -At "$@" -- "[ -f koronen.tmux ] || wget -O koronen.tmux https://raw.githubusercontent.com/Koronen/dotfiles/master/tmux.conf; tmux -f koronen.tmux -L koronen.tmux new-session -A -s 0"
-}
-if command -v compdef > /dev/null 2>&1; then
-  compdef tssh=ssh
-fi
