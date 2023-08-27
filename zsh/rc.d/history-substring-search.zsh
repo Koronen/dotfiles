9b069e38a96ac5d8a9fd280611f9f0db4d550ce0
Victor Koronen <git@victorkoronen.se>
2023-08-27 17:19:39 +0200

    Fix SC1087 ShellCheck error
        SC1087 (error): Use braces when expanding arrays,
        e.g. ${array[idx]} (or ${var}[.. to quiet).
    
    Ref: <https://github.com/koalaman/shellcheck/wiki/SC1087>

--- zsh/rc.d/history-substring-search.zsh
+++ zsh/rc.d/history-substring-search.zsh
@@ -2,8 +2,8 @@
 
 # bind UP and DOWN arrow keys
 zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey "${terminfo[kcuu1]}" history-substring-search-up
+bindkey "${terminfo[kcud1]}" history-substring-search-down
 
 # bind P and N for EMACS mode
 bindkey -M emacs '^P' history-substring-search-up
