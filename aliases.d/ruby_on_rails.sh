bc3b6ada5235a6265e6488bfea6a24ef092c13b2
Victor Koronen <git@victorkoronen.se>
2023-12-08 16:22:42 +0100

    Update Ruby on Rails aliases
    For modern versions of Ruby on Rails.

--- aliases.d/ruby_on_rails.sh
+++ aliases.d/ruby_on_rails.sh
@@ -1,41 +1,7 @@
 # shellcheck shell=sh
 
-rorc() {
-    if [ -x bin/rails ]; then
-        bin/rails console "$@"
-    else
-        bundle exec rails console "$@"
-    fi
-}
-
-rordm() {
-    if [ -x bin/rake ]; then
-        bin/rake db:migrate "$@"
-    else
-        bundle exec rake db:migrate "$@"
-    fi
-}
-
-rordr() {
-    if [ -x bin/rake ]; then
-        bin/rake db:rollback "$@"
-    else
-        bundle exec rake db:rollback "$@"
-    fi
-}
-
-rorro() {
-    if [ -x bin/rake ]; then
-        bin/rake routes "$@"
-    else
-        bundle exec rake routes "$@"
-    fi
-}
-
-rors() {
-    if [ -x bin/rails ]; then
-        bin/rails server --binding 127.0.0.1 "$@"
-    else
-        bundle exec rails server --binding 127.0.0.1 "$@"
-    fi
-}
+alias rorc='rails console'
+alias rordm='rails db:migrate'
+alias rordr='rails db:rollback'
+alias rorro='rails routes'
+alias rors='rails server'
