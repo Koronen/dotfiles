b7c2c2ac7108c6bb539c3d8dc9d7e8541f31e949
Victor Koronen <git@victorkoronen.se>
2023-12-28 15:26:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,14 @@ if [ "$CODESPACES" = "true" ]; then
   export RCRC="${DOTFILES_DIRS}/rcrc"
 
   sudo env DEBIAN_FRONTEND=noninteractive apt-get update
-  sudo env DEBIAN_FRONTEND=noninteractive apt-get install --yes rcm
+  sudo env DEBIAN_FRONTEND=noninteractive apt-get install --yes \
+    git \
+    rcm \
+    shellcheck \
+    yamllint
 
   rcup -f
+else
+  echo "Unsupported environment" >&2
+  exit 1
 fi
