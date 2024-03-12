30a95d64e38ff032d83714fecd4b5c72ee0e7896
Victor Koronen <git@victorkoronen.se>
2024-03-12 09:12:14 +0100

    Add more Ruby on Rails aliases

--- aliases.d/ruby_on_rails.sh
+++ aliases.d/ruby_on_rails.sh
@@ -1,8 +1,11 @@
 # shellcheck shell=sh
 
 alias rorc='rails console'
+alias rordc='rails dbconsole'
 alias rordm='rails db:migrate'
+alias rordmr='rails db:migrate:redo'
 alias rordr='rails db:rollback'
+alias rordR='rails db:reset'
 alias rorg='rails generate'
 alias rorro='rails routes'
 alias rors='rails server'
