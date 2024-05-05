d8aaf5bf939df9689f18e259e1f95d2a9e40084d
Victor Koronen <git@victorkoronen.se>
2024-05-05 12:13:14 +0200

    Move tmuxinator aliases to workstation tag

--- aliases.d/tmux.sh
+++ aliases.d/tmux.sh
@@ -1,8 +1,4 @@
 # shellcheck shell=sh
 
-alias mux=tmuxinator
-
 alias t=tmux
 alias ta='tmux attach'
-alias tsd='tmuxinator start dotfiles'
-alias tswp='tmuxinator start workstation-playbook'
---
+++ tag-workstation/aliases.d/tmuxinator.sh
@@ -0,0 +1,6 @@
+# shellcheck shell=sh
+
+alias mux=tmuxinator
+
+alias tsd='tmuxinator start dotfiles'
+alias tswp='tmuxinator start workstation-playbook'
