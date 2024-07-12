c618de6aca4a5e473aa32a932ca5348193a85cc9
Victor Koronen <git@victorkoronen.se>
2024-07-12 17:41:45 +0200

    Add host-specific directories for Codespaces and dev containers

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ set -e
 if [ "$CODESPACES" = "true" ]; then
   # shellcheck disable=SC2155
   export DOTFILES_DIRS="$(pwd)"
-  export RCRC="${DOTFILES_DIRS}/rcrc"
+  export RCRC="${DOTFILES_DIRS}/host-codespace/rcrc"
 
   sudo env DEBIAN_FRONTEND=noninteractive apt-get update
   sudo env DEBIAN_FRONTEND=noninteractive apt-get install --yes git rcm
 
-  rcup -f
+  rcup -f -v
 elif [ "$REMOTE_CONTAINERS" = "true" ]; then
-  export RCRC="$HOME/.dotfiles/rcrc"
+  export RCRC="$HOME/.dotfiles/host-remote-container/rcrc"
 
   sudo env DEBIAN_FRONTEND=noninteractive apt-get update
   sudo env DEBIAN_FRONTEND=noninteractive apt-get install --yes git rcm
 
-  rcup -f
+  rcup -f -v
 else
   echo "Unsupported environment" >&2
   exit 1
