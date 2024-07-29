69335c8f20f831a8f1136d6f88cd84d30c93e159
Victor Koronen <git@victorkoronen.se>
2024-07-29 16:33:50 +0200

    Add 'dce' alias

--- aliases.d/docker.sh
+++ aliases.d/docker.sh
@@ -3,9 +3,11 @@
 alias d='docker'
 alias di='docker images'
 alias dl='docker ps'
+
 alias dc='docker compose'
 alias dcb='docker compose build'
 alias dcd='docker compose down --remove-orphans'
+alias dce='docker compose exec'
 alias dcl='docker compose ps'
 alias dcp='docker compose pull'
 alias dcu='docker compose up --detach --remove-orphans'
