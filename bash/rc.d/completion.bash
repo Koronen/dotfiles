0e414d8eab5ccf724583a163821433189a0434f4
Victor Koronen <git@victorkoronen.se>
2025-03-02 20:28:57 +0100

    Support asdf-vm v0.16+ completions
    In preparation for upgrading asdf-vm to v0.16+.
    
    Ref: <https://asdf-vm.com/guide/getting-started.html#_2-configure-asdf>

--- bash/rc.d/completion.bash
+++ bash/rc.d/completion.bash
@@ -2,6 +2,11 @@ if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     source /etc/bash_completion
 fi
 
+if command -v asdf >/dev/null && [ "$(command -v asdf)" != "$HOME/.asdf/bin/asdf" ]; then
+    # shellcheck source=/dev/null
+    source <(asdf completion bash)
+fi
+
 if command -v gh >/dev/null; then
     eval "$(gh completion --shell bash)"
 fi
