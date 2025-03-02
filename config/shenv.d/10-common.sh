e04e3aa57d255812f6e239a0750c7cdf56f26da3
Victor Koronen <git@victorkoronen.se>
2025-03-02 20:21:53 +0100

    Move some environment configuration to workstation tag

--- config/shenv.d/10-common.sh
+++ config/shenv.d/10-common.sh
@@ -1,13 +1,9 @@
 # shellcheck shell=sh
 
 export EDITOR='vim'
-export ERL_AFLAGS='-kernel shell_history enabled'
 export GATSBY_TELEMETRY_DISABLED=1
 export GPG_TTY="$(tty)"
-export HOMEBREW_NO_ANALYTICS=1
-export HOMEBREW_NO_AUTO_UPDATE=1
 export HUSKY_SKIP_INSTALL=1
-export KERL_BUILD_DOCS='yes'
 export LESS='-FRX'
 export NEXT_TELEMETRY_DISABLED=1
 export PAGER='less'
--- tag-workstation/config/shenv.d/30-workstation.sh
+++ tag-workstation/config/shenv.d/30-workstation.sh
@@ -1,3 +1,7 @@
 # shellcheck shell=sh
 
 export ASDF_DATA_DIR="$HOME/.asdf"
+export ERL_AFLAGS='-kernel shell_history enabled'
+export HOMEBREW_NO_ANALYTICS=1
+export HOMEBREW_NO_AUTO_UPDATE=1
+export KERL_BUILD_DOCS='yes'
