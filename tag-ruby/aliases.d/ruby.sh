7f09509fa697beeb901341335713b305664fd7fe
Victor Koronen <git@victorkoronen.se>
2025-03-27 20:00:25 +0100

    Add 'rbcag' and 'rbcaG' aliases

--- tag-ruby/aliases.d/ruby.sh
+++ tag-ruby/aliases.d/ruby.sh
@@ -7,6 +7,8 @@ alias rbbu='bundle update'
 
 alias rbc=bin/rubocop
 alias rbca='bin/rubocop --autocorrect'
+alias rbcag='bin/rubocop --autocorrect $(git status --porcelain | grep -v "^ \?D" | cut -b 4-)'
+alias rbcaG='bin/rubocop --autocorrect $(git diff-tree --no-commit-id --name-only --diff-filter=d -r HEAD)'
 
 alias rbt=bin/standardrb
 alias rbtf='bin/standardrb --fix'
