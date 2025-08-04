4afe26204d65b5313e8869deac33b3971649686e
Victor Koronen <git@victorkoronen.se>
2025-08-04 10:35:53 +0200

    Remove 'rordc' alias

--- tag-ruby/aliases.d/ruby_on_rails.sh
+++ tag-ruby/aliases.d/ruby_on_rails.sh
@@ -1,7 +1,6 @@
 # shellcheck shell=sh
 
 alias rorc='bin/rails console'
-alias rordc='bin/rails dbconsole'
 alias rordm='bin/rails db:migrate'
 alias rordmr='bin/rails db:migrate:redo'
 alias rordr='bin/rails db:rollback'
