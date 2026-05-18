542be76ac6068e675608b5c4734973a9fa44fa37
Victor Koronen <git@victorkoronen.se>
2026-05-18 20:20:11 +0200

    Move telemetry opt-outs

--- config/shenv.d/10-common.sh
+++ config/shenv.d/10-common.sh
@@ -1,11 +1,7 @@
 # shellcheck shell=sh
 
 export EDITOR='vim'
-export GATSBY_TELEMETRY_DISABLED=1
 export GPG_TTY="$(tty)"
-export HUSKY_SKIP_INSTALL=1
 export LESS='-FRX'
-export NEXT_TELEMETRY_DISABLED=1
 export PAGER='less'
-export STORYBOOK_DISABLE_TELEMETRY=1
 export VISUAL='view'
--- tag-workstation/config/shenv.d/30-workstation.sh
+++ tag-workstation/config/shenv.d/30-workstation.sh
@@ -2,7 +2,6 @@
 
 export ELECTRON_OZONE_PLATFORM_HINT=auto
 export ERL_AFLAGS='-kernel shell_history enabled'
-export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HUSKY_SKIP_INSTALL=1
 export KERL_BUILD_DOCS='yes'
-export MISE_USE_VERSIONS_HOST_TRACK=0
