e69892a0dff6147bad2adc9eaed724667c58f287
Victor Koronen <git@victorkoronen.se>
2026-08-26 10:42:12 +0200

    Rework bash completion generation
    Generate completion files like mise.
    
    Ref:
    <https://github.com/jdx/mise/blob/v2026.8.13/docs/cli/completion.md>

--- bash/rc.d/completion.bash
+++ bash/rc.d/completion.bash
@@ -1,15 +1,3 @@
 if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
   source /etc/bash_completion
 fi
-
-if command -v gh >/dev/null; then
-  eval "$(gh completion --shell bash)"
-fi
-
-if command -v kubectl >/dev/null; then
-  source <(kubectl completion bash)
-fi
-
-if command -v sops >/dev/null; then
-  source <(sops completion bash)
-fi
